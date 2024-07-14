74f84fe5494750ea7335e377979fce55ba0f4ba2
LEEDAEJIN <ldj4241@naver.com>
2024-07-14 20:34:55 +0900

    [프로젝트 세팅] employee 영속화 구현 1:mysql, docker 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,9 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
